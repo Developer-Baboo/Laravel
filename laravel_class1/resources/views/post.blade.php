ac0c19aafed55c88d65d8b0e3d2ddf4921935067
Developer Baboo <babooheerani@gmail.com>
2023-08-18 13:57:54 +0500

    added

---
+++ laravel_class1/resources/views/post.blade.php
@@ -0,0 +1,36 @@
+{{-- <h1>Post Page</h1> --}}
+
+{{-- <a href="/">Home</a>
+<a href="/about">About</a>
+<a href="/firstpost">First Post</a> --}}
+
+
+
+{{-- Tutoraila 9  --}}
+{{-- @include('pages.header')
+            <article>
+                <h1>Post Page</h1>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti provident enim facere. Quasi minus iure provident vero voluptate quod consequuntur sequi consectetur. Assumenda possimus voluptatem pariatur nam, deserunt distinctio molestias.
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Omnis sequi maiores quasi laboriosam delectus eveniet facilis fugit perspiciatis dolore. Culpa maxime veniam neque quibusdam praesentium, minus hic dolorum. Temporibus, aliquid.
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aut fugiat iste ipsum saepe ab exercitationem perspiciatis. Delectus ipsam dolor nesciunt quam et voluptas, voluptates quos expedita voluptatem. Temporibus, ratione unde.
+                </p>
+            </article>
+            @include('pages.sidebar'); 
+            @include('pages.footer'); --}}
+
+        
+{{-- Tutorial 10 --}}
+{{-- @extends('layouts.masterlayout');
+@section('content')
+<h1>post Page</h1>
+<p>
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit libero quidem maiores corporis harum, ut consequuntur, iusto placeat itaque perferendis molestiae suscipit temporibus officia quasi quibusdam sequi labore, dolor eligendi.
+</p>
+@endsection
+
+@section('sidebar')
+@parent
+<p>This is appended to the master sidebar </p>
+@endsection --}}
+
+l
